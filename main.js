1a2289acecf5d7e41207eb34b57d9c7986a526a7
jalilhashemi <jalil.hashemi@hotmail.com>
2025-12-20 14:15:16 +0100

    change the whole site

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 document.getElementById('year').textContent = new Date().getFullYear();
+
+document.addEventListener("DOMContentLoaded", () => {
+    const element = document.getElementById("typing-text");
+    const text = element.getAttribute("data-text");
+    let index = 0;
+
+    element.textContent = "\u00A0";
+
+    setTimeout(() => {
+        element.textContent = "";
+        function type() {
+            if (index < text.length) {
+                element.textContent += text.charAt(index);
+                index++;
+                setTimeout(type, 100); // typing speed (ms)
+            }
+        }
+
+        type();
+    }, 2000);
+});
